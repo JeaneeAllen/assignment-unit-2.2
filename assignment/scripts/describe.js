c1c587b2eba1b58ff425d141f6e05839ea43f52d
JeaneeAllen <156607224+JeaneeAllen@users.noreply.github.com>
2024-06-10 23:35:10 -0500

    Reviewed answers & made update

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,14 +26,14 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// We make a variable called name and set it to Mary as a name.
+// We make a variable called name and set it to Dane as a name.
 // Then ask for persons name.
 // If name is Mary console.log ('Hi, Mary!')
 // If name is not Mary console.log ('How do you do?')
 
 //CODE
 /*
-let name = 'Mary';
+let name = 'Dane';
 
 if (name === 'Mary') {
   console.log('Hi, Mary!');
